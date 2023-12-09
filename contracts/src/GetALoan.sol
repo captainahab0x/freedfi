9c52a6c89ff1f6b74252ffb9cc7dd4ca60c9db56
Garv Agarwal <114938722+garv-aga@users.noreply.github.com>
2023-12-09 22:14:58 +0530

    faulty contract

---
+++ contracts/src/GetALoan.sol
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import "./PoolController.sol";
+
+contract LendingPlatform {
+    enum Status {
+        Granted,
+        NotGranted,
+        Repaid
+    }
+
+    address public trader;
+
+    constructor(address _trader) {
+        trader = _trader;
+    }
+
+    PoolController poolController = new PoolController(trader, address(this));
+
+    mapping(address => mapping(address => uint256)) public borrowedAmounts;
+    mapping(address => bool) public hasActiveLoan;
+    mapping(address => mapping(uint256 => Status)) public market;
+    mapping(address => bool) public investers;
+
+    event Borrowed(address indexed lender, address indexed borrower, uint256 amount);
+    event Repaid(address indexed lender, address indexed borrower, uint256 amount);
+
+    function addInvester(address lender) external {
+        require(msg.sender == trader);
+        investers[lender] = true;
+    }
+
+    function getInvesters(address lender) public view returns (bool) {
+        if (investers[lender]) return true;
+        return false;
+    }
+
+    modifier onlyInvesters() {
+        require(investers[msg.sender] == true);
+        _;
+    }
+
+    function borrowRequest(address payable borrower, uint256 requestedAmount) external {
+        require(requestedAmount > 0, "Borrowed amount must be greater than zero");
+        require(!hasActiveLoan[borrower], "Borrower can only borrow one loan at a time");
+
+        market[borrower][requestedAmount] = Status.NotGranted;
+    }
+
+    function borrowApprove(address payable borrower, uint256 amount) external onlyInvesters {
+        // Effects
+        borrowedAmounts[borrower][msg.sender] = amount;
+        hasActiveLoan[borrower] = true;
+        market[borrower][amount] = Status.Granted;
+
+        // Interactions
+        poolController.sendLoan(borrower, payable(msg.sender), amount);
+
+        emit Borrowed(msg.sender, borrower, amount);
+    }
+
+    function repay(address payable lender, address payable borrower) external payable {
+        uint256 borrowedAmount = borrowedAmounts[borrower][lender];
+        require(borrowedAmount > 0, "Borrower has no borrowed amount to repay");
+        require(msg.sender == borrower, "Only borrower can repay");
+        require(msg.value == borrowedAmount, "You must repay the full borrowed amount");
+        // Effects
+        borrowedAmounts[borrower][lender] = 0;
+        hasActiveLoan[borrower] = false;
+        market[borrower][borrowedAmount] = Status.Repaid;
+
+        // Interactions
+        lender.transfer(borrowedAmount); // Send the borrowed amount to the borrower
+
+        emit Repaid(lender, borrower, borrowedAmount);
+    }
+
+    function getBorrowedAmount(address payable lender) public view returns (uint256) {
+        return borrowedAmounts[msg.sender][lender];
+    }
+}
