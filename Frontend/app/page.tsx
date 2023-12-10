f029437b1bd8cab540a4bd24a87d21d7d0f7b88e
Balaganesh K <k.balaganesh26@gmail.com>
2023-12-10 09:17:35 +0530

    removed the unused files

--- Frontend/app/page.tsx
+++ Frontend/app/page.tsx
@@ -1,8 +1,5 @@
-import Image from 'next/image';
-import { Button } from '@/components/ui/button';
 import HeroSection from '@/components/hero-section';
 import HowWorksSection from '@/components/how-works-section';
-import App from 'next/app';
 import ApplyForLoanSection from '@/components/apply-for-loan-section';
 import DisclaimerSection from '@/components/disclaimer-section';
 import FooterSection from '@/components/footer-section';
