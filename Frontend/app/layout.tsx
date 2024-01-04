9f416361a40c92a0ef254aee06138d9bd53c5ede
Justin Obi <justin@Justins-MacBook-Pro.local>
2024-01-04 12:10:20 +0100

    WIP

--- Frontend/app/layout.tsx
+++ Frontend/app/layout.tsx
@@ -1,7 +1,35 @@
 'use client';
 import './globals.css';
+import '@rainbow-me/rainbowkit/styles.css';
 import store from '@/store';
 import { Provider as ReduxProvider } from 'react-redux';
+
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import { arbitrumSepolia } from 'wagmi/chains';
+import { publicProvider } from 'wagmi/providers/public';
+
+const projectId = process.env.NEXT_PUBLIC_PROJECTID as string
+
+const { chains, publicClient } = configureChains(
+  [arbitrumSepolia],
+  [ publicProvider() ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'FreedFi',
+  projectId: 'YOUR_PROJECT_ID',
+  chains
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient
+})
 
 export default function RootLayout({
   children,
@@ -13,7 +41,11 @@ export default function RootLayout({
       <head />
       <body>
         <ReduxProvider store={store}>
-          {children}
+          <WagmiConfig config={wagmiConfig}>
+            <RainbowKitProvider chains={chains}>
+              {children}
+            </RainbowKitProvider>
+          </WagmiConfig>
         </ReduxProvider>
       </body>
     </html>
